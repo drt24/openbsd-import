769ef1a114788d512851d9f7806c6b16d62e5ace
niklas <>
1995-12-22 01:41:55 +0000

    Imported from Cygnus byacc: Define YYEMPTY and YYLEX, because GCC uses them.

--- src/usr.bin/yacc/skeleton.c
+++ src/usr.bin/yacc/skeleton.c
@@ -21,7 +21,9 @@ char *banner[] =
     "#define YYBYACC 1",
     "#define YYMAJOR 1",
     "#define YYMINOR 9",
-    "#define yyclearin (yychar=(-1))",
+    "#define YYLEX yylex()",
+    "#define YYEMPTY -1",
+    "#define yyclearin (yychar=(YYEMPTY))",
     "#define yyerrok (yyerrflag=0)",
     "#define YYRECOVERING (yyerrflag!=0)",
     0
