12f653186b1babbb0e4c0f98d2355d065458b723
deraadt <>
1996-01-07 09:55:33 +0000

    pass a useful environment to ip-up/ip-down scripts; this is not a security problem different fix than from banshee@gabriella.resort.com; netbsd pr#1866

--- src/usr.sbin/pppd/main.c
+++ src/usr.sbin/pppd/main.c
@@ -863,7 +863,6 @@ run_program(prog, args, must_exist)
     int must_exist;
 {
     int pid;
-    char *nullenv[1];
 
     pid = fork();
     if (pid == -1) {
@@ -906,9 +905,7 @@ run_program(prog, args, must_exist)
 
 	/* SysV recommends a second fork at this point. */
 
-	/* run the program; give it a null environment */
-	nullenv[0] = NULL;
-	execve(prog, args, nullenv);
+	execve(prog, args);
 	if (must_exist || errno != ENOENT)
 	    syslog(LOG_WARNING, "Can't execute %s: %m", prog);
 	_exit(-1);
