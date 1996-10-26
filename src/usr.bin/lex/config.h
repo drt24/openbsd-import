bd4d680c596d83a94b8c36ed8503c17c18ae05fe
tholo <>
1996-10-26 21:50:30 +0000

    Don't use <malloc.h>

--- src/usr.bin/lex/config.h
+++ src/usr.bin/lex/config.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: config.h,v 1.2 1996/06/26 05:35:29 deraadt Exp $	*/
 
 /* config.h.  Generated automatically by configure.  */
 /* $Header$ */
@@ -13,7 +13,7 @@
 #define STDC_HEADERS 1
 
 /* Define if you have the <malloc.h> header file.  */
-#define HAVE_MALLOC_H 1
+/* #undef HAVE_MALLOC_H */
 
 /* Define if you have the <string.h> header file.  */
 #define HAVE_STRING_H 1
