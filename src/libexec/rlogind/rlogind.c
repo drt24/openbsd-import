eab69c3f47d79f07e6d0dc406bace3f5fbf57ec8
deraadt <>
1997-02-05 21:09:30 +0000

    do not warn about valid options; invalid options correctly quit

--- src/libexec/rlogind/rlogind.c
+++ src/libexec/rlogind/rlogind.c
@@ -258,8 +258,7 @@ doit(f, fromp)
 		}
 #ifdef IP_OPTIONS
 		{
-		u_char optbuf[BUFSIZ/3], *cp;
-		char lbuf[sizeof(optbuf)*3+1], *lp;
+		u_char optbuf[BUFSIZ/3];
 		int optsize = sizeof(optbuf), ipproto, i;
 		struct protoent *ip;
 
@@ -269,11 +268,6 @@ doit(f, fromp)
 			ipproto = IPPROTO_IP;
 		if (getsockopt(0, ipproto, IP_OPTIONS, (char *)optbuf,
 		    &optsize) == 0 && optsize != 0) {
-			for (lp = lbuf, i = 0; i < optsize; i++, lp += 3)
-				sprintf(lp, " %2.2x", optbuf[i]);
-			syslog(LOG_NOTICE,
-			    "Connection received using IP options (ignored):%s",
-			    lbuf);
 			for (i = 0; i < optsize; ) {
 				u_char c = optbuf[i];
 				if (c == IPOPT_LSRR || c == IPOPT_SSRR)
