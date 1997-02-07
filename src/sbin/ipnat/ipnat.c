f3e2f26c9d59c9a10a10f2e027164ef3142e9efd
kstailey <>
1997-02-07 18:10:07 +0000

    fix tags

--- src/sbin/ipnat/ipnat.c
+++ src/sbin/ipnat/ipnat.c
@@ -1,3 +1,4 @@
+/*	$OpenBSD$	*/
 /*
  * (C)opyright 1993,1994,1995 by Darren Reed.
  *
@@ -49,10 +50,10 @@
 #include "ip_nat.h"
 #include <ctype.h>
 
-
+#if 0
 #ifndef lint
 static  char    sccsid[] ="@(#)ipnat.c	1.9 6/5/96 (C) 1993 Darren Reed";
-static	char	rcsid[] = "$Id$";
+#endif
 #endif
 
 #if	SOLARIS
