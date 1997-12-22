58a5e9c47ec42430befe4d0aabe08aa761f9738d
brian <>
1997-12-22 02:28:33 +0000

    Don't forget to ntohs(sin.sin_port) (DUH!)

--- src/usr.sbin/ppp/main.c
+++ src/usr.sbin/ppp/main.c
@@ -981,10 +981,10 @@ DoLoop(void)
           LogPrintf(LogPHASE, "Connected to local client.\n");
           break;
         case AF_INET:
-          if (sin->sin_port < 1024) {
+          if (ntohs(sin->sin_port) < 1024) {
             LogPrintf(LogALERT, "Rejected client connection from %s:%u"
                       "(invalid port number) !\n",
-                      inet_ntoa(sin->sin_addr), sin->sin_port);
+                      inet_ntoa(sin->sin_addr), ntohs(sin->sin_port));
 	    close(wfd);
 	    continue;
           }
