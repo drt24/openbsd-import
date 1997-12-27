06a921676e4a8eaf12414d96dd6d6801504013ea
brian <>
1997-12-27 13:44:15 +0000

    Remove bogus timeout code in Receive(). Don't read(fd, buffer, 0) and think ppp has closed the connection when `buffer' is full, instead, flush most of buffer to the terminal and read() for a reasonable length. This fixes "show route" when there's more than 2k of routing output.

--- src/usr.sbin/pppctl/pppctl.c
+++ src/usr.sbin/pppctl/pppctl.c
@@ -99,12 +99,7 @@ Receive(int fd, int display)
         }
         len += Result;
         Buffer[len] = '\0';
-        if (TimedOut) {
-            if (display & REC_VERBOSE)
-                write(1,Buffer,len);
-            Result = -1;
-            break;
-        } else if (len > 2 && !strcmp(Buffer+len-2, "> ")) {
+        if (len > 2 && !strcmp(Buffer+len-2, "> ")) {
             prompt = strrchr(Buffer, '\n');
             if (display & (REC_SHOW|REC_VERBOSE)) {
                 if (display & REC_VERBOSE)
@@ -137,6 +132,17 @@ Receive(int fd, int display)
             } else
                 Result = 0;
             break;
+        }
+        if (len == sizeof Buffer - 1) {
+            int flush;
+            if ((last = strrchr(Buffer, '\n')) == NULL)
+                /* Yeuch - this is one mother of a line ! */
+                flush = sizeof Buffer / 2;
+            else
+                flush = last - Buffer + 1;
+            write(1, Buffer, flush);
+            strcpy(Buffer, Buffer + flush);
+            len -= flush;
         }
     }
 
