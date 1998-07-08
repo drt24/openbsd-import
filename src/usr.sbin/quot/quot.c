2c261bc5500edb6d3423cd9c1f4d0951f256019f
millert <>
1998-07-08 19:43:23 +0000

    When multiplying bumber of blocks by DEV_BSIZE, cast nblocks to off_t so we don't wrap.

--- src/usr.sbin/quot/quot.c
+++ src/usr.sbin/quot/quot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: quot.c,v 1.4 1996/06/10 08:32:15 deraadt Exp $	*/
 /*	$NetBSD: quot.c,v 1.7.4.1 1996/05/31 18:06:36 jtc Exp $	*/
 
 /*
@@ -70,7 +70,7 @@ static int headerlen;
 #ifdef	COMPAT
 #define	SIZE(n)	(n)
 #else
-#define	SIZE(n)	howmany((n) * DEV_BSIZE, blocksize)
+#define	SIZE(n)	howmany(((off_t)(n)) * DEV_BSIZE, blocksize)
 #endif
 
 #define	INOCNT(fs)	((fs)->fs_ipg)
