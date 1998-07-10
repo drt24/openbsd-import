8a3cd395a91a2c9e1fb30c1ab6263c46fbb76a18
provos <>
1998-07-10 20:27:42 +0000

    fix va_arg handling when not __STDC__

--- src/sbin/ipsec/photurisd/errlog.c
+++ src/sbin/ipsec/photurisd/errlog.c
@@ -31,7 +31,7 @@
  */
 
 /*
- * $OpenBSD: /cvs/src/sbin/ipsec/photurisd/errlog.c,v 1.4 1997/08/24 18:30:33 millert Exp $
+ * $OpenBSD: errlog.c,v 1.5 1997/09/03 08:44:41 provos Exp $
  */
 
 #ifndef lint
@@ -88,6 +88,7 @@ crit_error(flag, fmt, va_alist)
 	va_start(ap, fmt);
 #else
 	va_start(ap);
+	fmt = va_arg (ap, char *);
 #endif
 	_log_error(flag, fmt, ap);
 	va_end(ap);
@@ -114,6 +115,7 @@ log_error(flag, fmt, va_alist)
      va_start(ap, fmt);
 #else
      va_start(ap);
+     fmt = va_arg (ap, char *);
 #endif
      _log_error(flag, fmt, ap);
      va_end(ap);
