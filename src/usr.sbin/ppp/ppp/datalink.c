7f93fffc7f8e05e9a0bba78318e0e2e071f284f8
brian <>
1999-01-20 18:07:11 +0000

    Recurse when we've switched state via LoginDone().  If we've got an open link, we want it to be select()d on - otherwise we get a freeze when ``openmode'' is passive.

--- src/usr.sbin/ppp/ppp/datalink.c
+++ src/usr.sbin/ppp/ppp/datalink.c
@@ -305,7 +305,7 @@ datalink_UpdateSet(struct descriptor *d, fd_set *r, fd_set *w, fd_set *e,
             case DATALINK_LOGIN:
               dl->phone.alt = NULL;
               datalink_LoginDone(dl);
-              break;
+              return datalink_UpdateSet(d, r, w, e, n);
           }
           break;
         case CHAT_FAILED:
