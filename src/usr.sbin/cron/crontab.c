3516f231ec69864c2e85fcf3ecac5a4093dec34c
millert <>
1999-05-29 18:51:14 +0000

    Check size as well as mtime when deciding whether or not the file was changed during edit.  Crontab patch from <andrew@untraceable.net>. It may be better to simply do a real compare inline.

--- src/usr.sbin/cron/crontab.c
+++ src/usr.sbin/cron/crontab.c
@@ -310,6 +310,7 @@ edit_cmd() {
 	int		ch, t, x;
 	struct stat	statbuf;
 	time_t		mtime;
+	off_t		size;
 	WAIT_T		waiter;
 	PID_T		pid, xpid;
 
@@ -388,6 +389,7 @@ edit_cmd() {
 		goto fatal;
 	}
 	mtime = statbuf.st_mtime;
+	size = statbuf.st_size;
 
 	if ((!(editor = getenv("VISUAL")))
 	 && (!(editor = getenv("EDITOR")))
@@ -468,7 +470,7 @@ edit_cmd() {
 		perror("fstat");
 		goto fatal;
 	}
-	if (mtime == statbuf.st_mtime) {
+	if (mtime == statbuf.st_mtime && size == statbuf.st_size) {
 		fprintf(stderr, "%s: no changes made to crontab\n",
 			ProgramName);
 		goto remove;
