f775e10cfebcbf307f1ede4d7d70b7f47ac252a5
brian <>
1999-07-27 23:47:21 +0000

    o Overhaul filtering, adding facilities to jump over rules and to   negate the sense of rules. o Remove the redundant (and undocumented) ``host'' and ``port''   words (README.changes updated). o Don't permit (and ignore) garbage instead of the protocol.
    Mostly submitted by:  Peter Jeremy <jeremyp@gsmx07.alcatel.com.au>

--- src/usr.sbin/ppp/ppp/bundle.c
+++ src/usr.sbin/ppp/ppp/bundle.c
@@ -854,6 +854,15 @@ bundle_Create(const char *prefix, int type, const char **argv)
   bundle.filter.dial.logok = 1;
   bundle.filter.alive.name = "ALIVE";
   bundle.filter.alive.logok = 1;
+  {
+    int	i;
+    for (i = 0; i < MAXFILTERS; i++) {
+        bundle.filter.in.rule[i].f_action = A_NONE;
+        bundle.filter.out.rule[i].f_action = A_NONE;
+        bundle.filter.dial.rule[i].f_action = A_NONE;
+        bundle.filter.alive.rule[i].f_action = A_NONE;
+    }
+  }
   memset(&bundle.idle.timer, '\0', sizeof bundle.idle.timer);
   bundle.idle.done = 0;
   bundle.notify.fd = -1;
