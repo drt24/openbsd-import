1118136bec812686943d87cf9965af2f3a8f371d
deraadt <>
1999-10-19 15:56:41 +0000

    stop leak; after branch, builders take note

--- src/usr.bin/ssh/auth-passwd.c
+++ src/usr.bin/ssh/auth-passwd.c
@@ -36,13 +36,13 @@ int auth_password(struct passwd *pw, const char *password)
 
   if (pw->pw_uid == 0 && options.permit_root_login == 2)
   {
-      packet_send_debug("Server does not permit root login with password.");
+      /*packet_send_debug("Server does not permit root login with password.");*/
       return 0;
   }
 
   if (*password == '\0' && options.permit_empty_passwd == 0)
   {
-      packet_send_debug("Server does not permit empty password login.");
+      /*packet_send_debug("Server does not permit empty password login.");*/
       return 0;
   }
 
