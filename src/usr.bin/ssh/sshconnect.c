0d9863fa7a9ad956ece5f17d35b560c4e40f2164
markus <>
1999-10-25 20:34:30 +0000

    bugfix: print remote version, from chris@dqc.org

--- src/usr.bin/ssh/sshconnect.c
+++ src/usr.bin/ssh/sshconnect.c
@@ -925,7 +925,7 @@ void ssh_exchange_identification()
     enable_compat13();
     if (options.forward_agent && strcmp(remote_version, SSH_VERSION) != 0) {
       log("Agent forwarding disabled, remote version '%s' is not compatible.",
-	    SSH_VERSION);
+	    remote_version);
       options.forward_agent = 0;
     }
   }
