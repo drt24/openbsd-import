af6a337110bc68c3af380ba35a418f2c575be326
markus <>
1999-11-24 20:26:35 +0000

    progress meter overflow fix from damien@ibs.com.au

--- src/usr.bin/ssh/scp.c
+++ src/usr.bin/ssh/scp.c
@@ -1146,7 +1146,7 @@ progressmeter(int flag)
 	(void) gettimeofday(&now, (struct timezone *) 0);
 	cursize = statbytes;
 	if (totalbytes != 0) {
-		ratio = cursize * 100 / totalbytes;
+		ratio = 100.0 * cursize / totalbytes;
 		ratio = MAX(ratio, 0);
 		ratio = MIN(ratio, 100);
 	} else
