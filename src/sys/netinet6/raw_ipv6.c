45d4088c57ce43567d56899d008477164da04280
itojun <>
2000-02-28 11:55:23 +0000

    bring in recent KAME changes (only important and stable ones, as usual). - remove net.inet6.ip6.nd6_proxyall.  introduce proxy NDP code works   just like "arp -s". - revise source address selection.   be more careful about use of yet-to-be-valid addresses as source. - as router, transmit ICMP6_DST_UNREACH_BEYONDSCOPE against out-of-scope   packet forwarding attempt. - path MTU discovery takes care of routing header properly. - be more strict about mbuf chain parsing. - nuke xxCTL_VARS #define, they are for BSDI. - disable SIOCSIFDSTADDR_IN6/SIOCSIFNETMASK_IN6 ioctl, they do not fit   IPv6 model where multiple address on interface is normal.   (kernel side supports them for a while for backward compat,   the support will be nuked shortly) - introduce "default outgoing interface" (for spec conformance in very   rare case)

--- src/sys/netinet6/raw_ipv6.c
+++ src/sys/netinet6/raw_ipv6.c
@@ -1,4 +1,5 @@
-/* $OpenBSD: raw_ipv6.c,v 1.12 2000/01/05 17:30:52 itojun Exp $ */
+/*	$OpenBSD: raw_ipv6.c,v 1.13 2000/02/07 06:09:10 itojun Exp $	*/
+
 /*
 %%% copyright-nrl-95
 This software is Copyright 1995-1998 by Randall Atkinson, Ronald Lee,
