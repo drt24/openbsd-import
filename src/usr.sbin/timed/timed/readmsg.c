ea758db0785157cfa5f21ba603eba56304ab1bb6
fgsch <>
2001-03-31 19:38:59 +0000

    Security fixes: check for short packets and bad types; from FreeBSD. millert@ ok.

--- src/usr.sbin/timed/timed/readmsg.c
+++ src/usr.sbin/timed/timed/readmsg.c
@@ -85,6 +85,7 @@ readmsg(int type, char *machfrom, struct timeval *intvl,
 	struct tsplist *prev;
 	register struct netinfo *ntp;
 	register struct tsplist *ptr;
+	ssize_t n;
 
 	if (trace) {
 		fprintf(fd, "readmsg: looking for %s from %s, %s\n",
@@ -203,11 +204,17 @@ again:
 			continue;
 		}
 		length = sizeof(from);
-		if (recvfrom(sock, (char *)&msgin, sizeof(struct tsp), 0,
-			     (struct sockaddr*)&from, &length) < 0) {
+		if ((n = recvfrom(sock, (char *)&msgin, sizeof(struct tsp), 0,
+			     (struct sockaddr*)&from, &length)) < 0) {
 			syslog(LOG_ERR, "recvfrom: %m");
 			exit(1);
 		}
+		if (n < sizeof(struct tsp)) {
+			syslog(LOG_NOTICE, "short packet (%u/%u bytes) from %s",
+			    n, sizeof(struct tsp), inet_ntoa(from.sin_addr));
+			continue;
+		}
+
 		(void)gettimeofday(&from_when, (struct timezone *)0);
 		bytehostorder(&msgin);
 
@@ -441,6 +448,12 @@ struct sockaddr_in *addr;
 {
 	char tm[26];
 	time_t msgtime;
+	
+	if (msg->tsp_type > TSPTYPENUMBER) {
+		fprintf(fd, "bad type (%u) on packet from %s\n",
+		    msg->tsp_type, inet_ntoa(addr->sin_addr));
+		return;
+	}
 
 	switch (msg->tsp_type) {
 
