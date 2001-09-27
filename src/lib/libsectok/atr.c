1dbe6afdbb6ac3cc669b4f7c293da078ebf3ca36
rees <>
2001-09-27 16:01:41 +0000

    hush gcc warning

--- src/lib/libsectok/atr.c
+++ src/lib/libsectok/atr.c
@@ -218,18 +218,17 @@ sectok_parse_atr(int ttyn, int flags, unsigned char *atr, int len, struct scpara
     if (nhb) {
 	hb = ap;
 	ap += nhb;
-    }
-
-    if (nhb && (flags & STRV)) {
-	printf("%d historical bytes:", nhb);
-	for (i = 0; i < nhb; i++) {
-	    c = hb[i];
-	    if (c >= ' ' && c <= '~')
-		printf(" %c", c);
-	    else
-		printf(" %02x", c);
+	if (flags & STRV) {
+	    printf("%d historical bytes:", nhb);
+	    for (i = 0; i < nhb; i++) {
+		c = hb[i];
+		if (c >= ' ' && c <= '~')
+		    printf(" %c", c);
+		else
+		    printf(" %02x", c);
+	    }
+	    printf("\n");
 	}
-	printf("\n");
     }
 
     if (hiproto > 0)
