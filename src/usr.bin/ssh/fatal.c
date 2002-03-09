1851e5ef27deafb06a571eb2940b49d8eaff4495
miod <>
2002-03-09 00:20:50 +0000

    Merge OpenSSH 3.1, keeping /etc as configuration files directory. (i.e. OpenSSH 3.1 + openbsd29_3.1.patch)

--- src/usr.bin/ssh/fatal.c
+++ src/usr.bin/ssh/fatal.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: log.c,v 1.21 2002/02/04 12:15:25 markus Exp $");
+RCSID("$OpenBSD: fatal.c,v 1.1 2002/02/22 12:20:34 markus Exp $");
 
 #include "log.h"
 
