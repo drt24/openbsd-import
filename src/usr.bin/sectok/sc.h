ffcbee1b5e608efae9de532d8f98c78fceda8fc9
rees <>
2002-03-14 21:11:50 +0000

    Add 'pin' and 'chpin' commands

--- src/usr.bin/sectok/sc.h
+++ src/usr.bin/sectok/sc.h
@@ -56,6 +56,8 @@ int class(int ac, char *av[]);
 int dread(int ac, char *av[]);
 int dwrite(int ac, char *av[]);
 int challenge(int ac, char *av[]);
+int vfypin(int ac, char *av[]);
+int chpin(int ac, char *av[]);
 int ls(int ac, char *av[]);
 int acl(int ac, char *av[]);
 int jcreate(int ac, char *av[]);
