44812562aa174752b29003feaec6f709495e63ff
niklas <>
2002-03-28 10:36:04 +0000

    Merge in -current from about a week ago

--- src/sys/arch/mvme88k/dev/busswfunc.h
+++ src/sys/arch/mvme88k/dev/busswfunc.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD: busswfunc.h,v 1.1 2001/12/13 08:55:51 smurph Exp $ */
+/*	$OpenBSD$ */
 
 #ifndef _MVME88K_BUSSWF_H_
 #define _MVME88K_BUSSWF_H_
 
-int busswintr_establish __P((int vec, struct intrhand *ih));
+int busswintr_establish(int vec, struct intrhand *ih);
 
 #endif	/* _MVME88K_PCCTWO_H_ */
 
