3a81a527a31e1949a6445e9431bf7284acbe3012
beck <>
2002-03-29 02:08:08 +0000

    fix half baked abortion of a merge to 1.3.23 and take tree to apache-1.3.24+mod+ssl2.8.8

--- src/usr.sbin/httpd/src/os/bs2000/ebcdic.h
+++ src/usr.sbin/httpd/src/os/bs2000/ebcdic.h
@@ -1,11 +1,6 @@
-#ifndef AP_EBCDIC_H
-#define AP_EBCDIC_H  "$Id$"
+#ifndef EBCDIC_H
+#define EBCDIC_H  "$Id$"
 
-#include <sys/types.h>
+#include <ap_ebcdic.h>
 
-extern const unsigned char os_toascii[256];
-extern const unsigned char os_toebcdic[256];
-API_EXPORT(void *) ebcdic2ascii(void *dest, const void *srce, size_t count);
-API_EXPORT(void *) ascii2ebcdic(void *dest, const void *srce, size_t count);
-
-#endif /*AP_EBCDIC_H*/
+#endif /*EBCDIC_H*/
