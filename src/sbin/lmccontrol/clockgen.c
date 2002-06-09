d9452cab3061c1a6bbcaf29ac4fea4649787d9e7
todd <>
2002-06-09 08:13:09 +0000

    rm trailing whitespace

--- src/sbin/lmccontrol/clockgen.c
+++ src/sbin/lmccontrol/clockgen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: clockgen.c,v 1.1 2000/10/13 16:22:07 chris Exp $ */
 /* $Id$ */
 
 /*-
@@ -6,7 +6,7 @@
  * All rights reserved.  www.lanmedia.com
  *
  * This code is written by Michael Graff (explorer@vix.com) for LMC.
- * The code is derived from permitted modifications to software created 
+ * The code is derived from permitted modifications to software created
  * by Matt Thomas (matt@3am-software.com).
  *
  * Redistribution and use in source and binary forms, with or without
@@ -17,7 +17,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All marketing or advertising materials mentioning features or use of this 
+ * 3. All marketing or advertising materials mentioning features or use of this
  *    software must display the following acknowledgement:
  *      This product includes software developed by LAN Media Corporation
  *      and its contributors.
