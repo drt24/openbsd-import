8696296cdc79006370dbfed9466bb22f63e38a62
niklas <>
2003-03-28 00:41:31 +0000

    Sync the SMP branch with 3.3

--- src/sys/xfs/xfs_syscalls.h
+++ src/sys/xfs/xfs_syscalls.h
@@ -14,12 +14,7 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- *
- * 4. Neither the name of the Institute nor the names of its contributors
+ * 3. Neither the name of the Institute nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -43,6 +38,8 @@
 
 #include <xfs/xfs_common.h>
 #include <xfs/xfs_message.h>
+
+#include <xfs/afssysdefs.h>
 
 struct sys_pioctl_args {
     syscallarg(int) operation;
