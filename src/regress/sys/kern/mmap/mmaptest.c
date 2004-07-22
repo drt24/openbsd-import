ccf38a5358a4ad76290b3f26a7b858ce94a97c10
miod <>
2004-07-22 15:11:37 +0000

    Spell reserved correctly; ok from copyright holders when in licence blocks.

--- src/regress/sys/kern/mmap/mmaptest.c
+++ src/regress/sys/kern/mmap/mmaptest.c
@@ -1,7 +1,7 @@
 /*	$Id$	*/
 
 /*
- * Copyright (c) 2001 Niklas Hallqvist.  All rights reserverd.
+ * Copyright (c) 2001 Niklas Hallqvist.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
