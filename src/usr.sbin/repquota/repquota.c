52beded654e436b36dab25148f2a83f756534332
deraadt <>
2005-04-01 04:31:11 +0000

    these commands said "blocks" when they meant "KBytes" (they even calculated it so).  pointed out by janc@cyberspace.org, pr 3956

--- src/usr.sbin/repquota/repquota.c
+++ src/usr.sbin/repquota/repquota.c
@@ -208,7 +208,7 @@ repquota(struct fstab *fs, int type, char *qfpathname)
 		fup->fu_dqblk = dqbuf;
 	}
 	fclose(qf);
-	printf("                        Block limits               File limits\n");
+	printf("                        KByte limits               File limits\n");
 	printf("User            used    soft    hard  grace    used  soft  hard  grace\n");
 	for (id = 0; id <= highid[type]; id++) {
 		fup = lookup(id, type);
