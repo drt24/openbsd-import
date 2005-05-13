ec12d1c0b205fc1bdf730ae9c3a601aa6acf8c3b
jmc <>
2005-05-13 13:16:13 +0000

    some Xr's from jason ackley;

--- src/usr.sbin/ypserv/ypinit/ypinit.sh
+++ src/usr.sbin/ypserv/ypinit/ypinit.sh
@@ -88,7 +88,7 @@ fi
 if [ -z "${DOMAIN}" ]; then
 	cat << \__no_domain 1>&2
 The local host's YP domain name has not been set.  Please set it with
-the domainname(8) command or pass the domain as an argument to ypinit(8).
+the domainname(1) command or pass the domain as an argument to ypinit(8).
 __no_domain
 
 	exit 1
@@ -99,7 +99,7 @@ HOST=`${HOSTNAME}`
 if [ -z "${HOST}" ]; then
 	cat << \__no_hostname 1>&2
 The local host's hostname has not been set.  Please set it with the
-hostname(8) command.
+hostname(1) command.
 __no_hostname
 
 	exit 1
