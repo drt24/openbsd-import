63a726ef457c6ae6e70944495687f7ef1e90d189
miod <>
2007-10-18 04:32:27 +0000

    No need to include <machine/pte.h> here.

--- src/sys/arch/mips64/mips64/process_machdep.c
+++ src/sys/arch/mips64/mips64/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: process_machdep.c,v 1.6 2005/12/17 14:56:31 kettenis Exp $	*/
+/*	$OpenBSD: process_machdep.c,v 1.7 2007/04/26 17:05:11 miod Exp $	*/
 
 /*
  * Copyright (c) 1994 Adam Glass
@@ -73,7 +73,6 @@
 #include <sys/user.h>
 #include <sys/vnode.h>
 #include <sys/ptrace.h>
-#include <machine/pte.h>
 #include <machine/psl.h>
 #include <machine/frame.h>
 
