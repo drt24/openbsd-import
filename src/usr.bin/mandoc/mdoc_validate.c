2af40eef96af2d4c117c0cb1badbaf618555185f
schwarze <>
2009-07-06 22:33:58 +0000

    remove unused WDEPCOL warning that became unused in 1.7.19 ok kristaps@  and contained in 1.7.21

--- src/usr.bin/mandoc/mdoc_validate.c
+++ src/usr.bin/mandoc/mdoc_validate.c
@@ -55,7 +55,6 @@ enum	mwarn {
 	WNOWIDTH,
 	WMISSWIDTH,
 	WESCAPE,
-	WDEPCOL,
 	WWRONGMSEC,
 	WSECOOO,
 	WSECREP,
@@ -462,10 +461,6 @@ pwarn(struct mdoc *m, int line, int pos, enum mwarn type)
 		break;
 	case (WPROLOOO):
 		p = "prologue macros out-of-order";
-		c = WARN_COMPAT;
-		break;
-	case (WDEPCOL):
-		p = "deprecated column argument syntax";
 		c = WARN_COMPAT;
 		break;
 	case (WNOWIDTH):
