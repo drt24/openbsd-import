f00880e6c634e5d257a982b07da6a69c9da397e5
schwarze <>
2010-05-14 14:47:44 +0000

    Merge 1.9.25, keeping local patches; this does not merge kristaps' end-of-sentences handling yet, i will check that separately.  This one includes: * handle \*(Ba as a delimiter * introduce ARGS_PEND for .Bl -column .It end-of-line special casing * section ordering: expect EXIT STATUS at the right place * line break fixes in SYNOPSIS * allow literal contexts to have arbitrary line lengths * the input file column number can not be used to identify the beginning   of a line because white space is allowed after the initial '.' * proper leading spaces in -man -Tascii mode * do not let Lb break lines in -mdoc -Thtml LIBRARY

--- src/usr.bin/mandoc/mdoc_argv.c
+++ src/usr.bin/mandoc/mdoc_argv.c
@@ -399,9 +399,10 @@ args(struct mdoc *m, int line, int *pos,
 		return(ARGS_EOLN);
 
 	/* 
-	 * If the first character is a delimiter and we're to look for
-	 * delimited strings, then pass down the buffer seeing if it
-	 * follows the pattern of [[::delim::][ ]+]+.
+	 * If the first character is a closing delimiter and we're to
+	 * look for delimited strings, then pass down the buffer seeing
+	 * if it follows the pattern of [[::delim::][ ]+]+.  Note that
+	 * we ONLY care about closing delimiters.
 	 */
 
 	if ((fl & ARGS_DELIM) && mdoc_iscdelim(buf[*pos]) > 1) {
@@ -409,14 +410,14 @@ args(struct mdoc *m, int line, int *pos,
 			if ( mdoc_iscdelim(buf[i]) < 2)
 				break;
 			i++;
-			if (0 == buf[i] || ' ' != buf[i])
+			if ('\0' == buf[i] || ' ' != buf[i])
 				break;
 			i++;
 			while (buf[i] && ' ' == buf[i])
 				i++;
 		}
 
-		if (0 == buf[i]) {
+		if ('\0' == buf[i]) {
 			*v = &buf[*pos];
 			if (' ' != buf[i - 1])
 				return(ARGS_PUNCT);
@@ -468,8 +469,10 @@ args(struct mdoc *m, int line, int *pos,
 		} else if (pp && ! p) {
 			p = pp;
 			*pos += 2;
-		} else
+		} else {
+			rc = ARGS_PEND;
 			p = strchr(*v, 0);
+		}
 
 		/* Whitespace check for eoln case... */
 		if (0 == *p && ' ' == *(p - 1) && ! (ARGS_NOWARN & fl))
