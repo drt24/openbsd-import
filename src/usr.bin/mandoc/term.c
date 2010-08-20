37464278657f02e0601cee9652fb5794c1108cb0
schwarze <>
2010-08-20 23:34:00 +0000

    Centralize handling of literal tabs in term_flushln() in one place, making the code simpler and easier to understand. No functional change.

--- src/usr.bin/mandoc/term.c
+++ src/usr.bin/mandoc/term.c
@@ -165,12 +165,11 @@ term_flushln(struct termp *p)
 		 * Handle literal tab characters: collapse all
 		 * subsequent tabs into a single huge set of spaces.
 		 */
-		for (j = i; j < (int)p->col; j++) {
-			if ('\t' != p->buf[j])
-				break;
+		while (i < (int)p->col && '\t' == p->buf[i]) {
 			vend = (vis / p->tabwidth + 1) * p->tabwidth;
 			vbl += vend - vis;
 			vis = vend;
+			i++;
 		}
 
 		/*
@@ -181,7 +180,7 @@ term_flushln(struct termp *p)
 		 */
 
 		/* LINTED */
-		for (jhy = 0; j < (int)p->col; j++) {
+		for (j = i, jhy = 0; j < (int)p->col; j++) {
 			if ((j && ' ' == p->buf[j]) || '\t' == p->buf[j])
 				break;
 
@@ -223,12 +222,6 @@ term_flushln(struct termp *p)
 				p->overstep;
 			p->overstep = 0;
 		}
-
-		/*
-		 * Skip leading tabs, they were handled above.
-		 */
-		while (i < (int)p->col && '\t' == p->buf[i])
-			i++;
 
 		/* Write out the [remaining] word. */
 		for ( ; i < (int)p->col; i++) {
