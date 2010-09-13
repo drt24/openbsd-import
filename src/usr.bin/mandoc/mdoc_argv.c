07d18029b63096d3c38093aa0b1ba1fd4c42c53b
schwarze <>
2010-09-13 20:10:11 +0000

    Prevent .Pp and .Lp from looping endlessly on punctuation. Fix for .Pp from kristaps@, i applied the same to .Lp. Bug reported by espie@.

--- src/usr.bin/mandoc/mdoc_argv.c
+++ src/usr.bin/mandoc/mdoc_argv.c
@@ -91,7 +91,7 @@ static	int mdoc_argflags[MDOC_MAX] = {
 	0, /* Os */
 	0, /* Sh */
 	0, /* Ss */ 
-	ARGS_DELIM, /* Pp */ 
+	0, /* Pp */ 
 	ARGS_DELIM, /* D1 */
 	ARGS_DELIM, /* Dl */
 	0, /* Bd */
@@ -192,7 +192,7 @@ static	int mdoc_argflags[MDOC_MAX] = {
 	0, /* Fr */
 	0, /* Ud */
 	0, /* Lb */
-	ARGS_DELIM, /* Lp */
+	0, /* Lp */
 	ARGS_DELIM, /* Lk */
 	ARGS_DELIM, /* Mt */
 	ARGS_DELIM, /* Brq */
