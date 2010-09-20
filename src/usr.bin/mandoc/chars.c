da5ce1493a193a7e28a7e10b781cbbe6b14f10d8
schwarze <>
2010-09-20 20:43:38 +0000

    remove remaining pod2man escapes, mandoc now uses the standard preamble; from kristaps@

--- src/usr.bin/mandoc/chars.c
+++ src/usr.bin/mandoc/chars.c
@@ -36,7 +36,7 @@ struct	ln {
 #define CHARS_BOTH	 (CHARS_CHAR | CHARS_STRING)
 };
 
-#define	LINES_MAX	  362
+#define	LINES_MAX	  351
 
 #define CHAR(in, ch, code) \
 	{ NULL, (in), (ch), (code), CHARS_CHAR },
@@ -90,13 +90,13 @@ chars_init(enum chars type)
 	tab = malloc(sizeof(struct tbl));
 	if (NULL == tab) {
 		perror(NULL);
-		exit(MANDOCLEVEL_SYSERR);
+		exit((int)MANDOCLEVEL_SYSERR);
 	}
 
 	htab = calloc(PRINT_HI - PRINT_LO + 1, sizeof(struct ln **));
 	if (NULL == htab) {
 		perror(NULL);
-		exit(MANDOCLEVEL_SYSERR);
+		exit((int)MANDOCLEVEL_SYSERR);
 	}
 
 	for (i = 0; i < LINES_MAX; i++) {
