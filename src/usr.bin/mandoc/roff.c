e5d623ad8eeed105fbd60437697ab24a910114cb
schwarze <>
2011-09-19 07:53:54 +0000

    Breaking the line at a hyphen is only allowed if the hyphen is both preceded and followed by an alphabetic character. This fixes about a dozen places in base.

--- src/usr.bin/mandoc/roff.c
+++ src/usr.bin/mandoc/roff.c
@@ -552,7 +552,6 @@ again:
 static enum rofferr
 roff_parsetext(char *p)
 {
-	char		 l, r;
 	size_t		 sz;
 	const char	*start;
 	enum mandoc_esc	 esc;
@@ -579,14 +578,8 @@ roff_parsetext(char *p)
 			continue;
 		}
 
-		l = *(p - 1);
-		r = *(p + 1);
-		if ('\\' != l &&
-				'\t' != r && '\t' != l &&
-				' ' != r && ' ' != l &&
-				'-' != r && '-' != l &&
-				! isdigit((unsigned char)l) &&
-				! isdigit((unsigned char)r))
+		if (isalpha((unsigned char)p[-1]) &&
+		    isalpha((unsigned char)p[1]))
 			*p = ASCII_HYPH;
 		p++;
 	}
