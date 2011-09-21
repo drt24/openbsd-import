3df78244e13773c023da7f8c7effa0acecac67e5
nicm <>
2011-09-21 06:26:51 +0000

    Add /usr/local/share/terminfo to the search path so that ports can install custom terminfo files. Minor ncurses bump to allow packages to depend on the right version. From discussion with zinke@ and dcoppa@.
    ok phessler dcoppa

--- src/lib/libcurses/ncurses_cfg.h
+++ src/lib/libcurses/ncurses_cfg.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ncurses_cfg.h,v 1.25 2010/09/06 17:26:17 nicm Exp $ */
+/* $OpenBSD: ncurses_cfg.h,v 1.26 2011/04/23 22:08:15 nicm Exp $ */
 
 /* include/ncurses_cfg.h.  Generated automatically by configure.  */
 /****************************************************************************
@@ -160,7 +160,7 @@
 #define STDC_HEADERS 1
 #define SYSTEM_NAME "openbsd"
 #define TERMINFO "/usr/share/terminfo"
-#define TERMINFO_DIRS "/usr/share/terminfo"
+#define TERMINFO_DIRS "/usr/share/terminfo:/usr/local/share/terminfo"
 #define TIME_WITH_SYS_TIME 1
 #define TYPEOF_CHTYPE long
 #define USE_ASSUMED_COLOR 1
