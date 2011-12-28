b02f017867fa3754d7f64b6e118f76fdf03d88f9
schwarze <>
2011-12-28 01:17:01 +0000

    Some parsing improvements from kristaps@: * Accept multiple lines in cat page NAME parsing. * Do not trim the description to 70 characters in the database. * Instead, limit the length of the description during output. * In man(7), accept \-\- as a Nm-Nd separator; occurs in 3p.

--- src/usr.bin/mandoc/apropos.c
+++ src/usr.bin/mandoc/apropos.c
@@ -123,11 +123,11 @@ list(struct res *res, size_t sz, void *arg)
 	qsort(res, sz, sizeof(struct res), cmp);
 
 	for (i = 0; i < (int)sz; i++)
-		printf("%s(%s%s%s) - %s\n", res[i].title,
+		printf("%s(%s%s%s) - %.*s\n", res[i].title,
 				res[i].cat,
 				*res[i].arch ? "/" : "",
 				*res[i].arch ? res[i].arch : "",
-				res[i].desc);
+				70, res[i].desc);
 }
 
 static int
