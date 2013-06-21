46920cf8ab73965734602dc7616283d394a3d25c
ajacoutot <>
2013-06-21 12:26:04 +0000

    digest.c is needed for some operations with the kdc(8).
    ok robert@

--- src/kerberosV/src/kdc/kdc_locl.h
+++ src/kerberosV/src/kdc/kdc_locl.h
@@ -43,6 +43,7 @@
 typedef struct pk_client_params pk_client_params;
 struct DigestREQ;
 struct Kx509Request;
+#include <kdc-private.h>
 
 extern sig_atomic_t exit_flag;
 extern size_t max_request_udp;
