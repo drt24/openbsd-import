f8e681db8fa1b7a9d8eed20b5e1ac816af0fed54
deraadt <>
2013-11-12 04:59:02 +0000

    add a variety of missing prototypes

--- src/sbin/lmccontrol/clockgen.c
+++ src/sbin/lmccontrol/clockgen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: clockgen.c,v 1.2 2002/06/09 08:13:07 todd Exp $ */
+/* $OpenBSD: clockgen.c,v 1.3 2004/08/08 19:04:25 deraadt Exp $ */
 /* $Id$ */
 
 /*-
@@ -52,6 +52,8 @@
 
 typedef struct lmc___ctl lmc_ctl_t;
 #include <dev/pci/if_lmcioctl.h>
+
+void lmc_av9110_freq(u_int32_t target, lmc_av9110_t *av);
 
 #define T1_FREF  20000000
 #define T1_FMIN  30000000
