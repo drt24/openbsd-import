65b40d40f3aab904c7ed49a7d05f3b5831c785da
lteo <>
2014-01-11 04:35:52 +0000

    Make icmp_print() accept the length variable, which is the length of the packet without the IP header.  This is needed by the next commit that will allow tcpdump to detect bad ICMP checksums.
    Related functions like {tcp,udp,icmp6}_print() already accept this
    length variable, so this change makes icmp_print() consistent with
    them as well.
    
    This commit makes no functional change to tcpdump itself.
    
    OK florian@

--- src/usr.sbin/tcpdump/interface.h
+++ src/usr.sbin/tcpdump/interface.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: interface.h,v 1.60 2010/01/12 06:10:33 naddy Exp $	*/
+/*	$OpenBSD: interface.h,v 1.61 2010/04/06 16:01:57 jsg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -210,7 +210,7 @@ extern void fddi_if_print(u_char *, const struct pcap_pkthdr *, const u_char *);
 extern void ppp_ether_if_print(u_char *, const struct pcap_pkthdr *,
 	const u_char *);
 extern void gre_print(const u_char *, u_int);
-extern void icmp_print(const u_char *, const u_char *);
+extern void icmp_print(const u_char *, u_int, const u_char *);
 extern void ieee802_11_if_print(u_char *, const struct pcap_pkthdr *,
     const u_char *);
 extern void ieee802_11_radio_if_print(u_char *, const struct pcap_pkthdr *,
