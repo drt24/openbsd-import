7d718234872e2621614aaac4c111ee78a27fe2b9
benno <>
2019-02-12 19:33:59 +0000

    sync a chunk that was lost, from commit 40bd0babafaa424d20af28128b76878d6cee7b87 Author: kristaps <> Date:   Mon Feb 11 23:27:36 2019 +0000
        Normal form.

--- src/usr.bin/rsync/flist.c
+++ src/usr.bin/rsync/flist.c
@@ -610,13 +610,13 @@ flist_recv(struct sess *sess, int fd, struct flist **flp, size_t *sz)
 		/* Conditional part: gid. */
 
 		if (sess->opts->preserve_gids) {
-			if ( ! (FLIST_GID_SAME & flag)) {
-				if ( ! io_read_int(sess, fd, &ival)) {
+			if (!(FLIST_GID_SAME & flag)) {
+				if (!io_read_int(sess, fd, &ival)) {
 					ERRX1(sess, "io_read_int");
 					goto out;
 				}
 				ff->st.gid = ival;
-			} else if (NULL == fflast) {
+			} else if (fflast == NULL) {
 				ERRX(sess, "same gid "
 					"without last entry");
 				goto out;
