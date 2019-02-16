901b761014a7983354fc69ddef8b3c42741459ea
florian <>
2019-02-16 10:49:37 +0000

    I did some work here.

--- src/usr.bin/rsync/receiver.c
+++ src/usr.bin/rsync/receiver.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2019 Florian Obser <florian@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
