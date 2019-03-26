8007dfe292ed6a5cb763bb60dd0b11b9cbab555d
deraadt <>
2019-03-26 16:20:03 +0000

    INT32_MAX isn't -1, to indicate re-read for 64-bit value. Diagnosed by krw, ok florian

--- src/usr.bin/rsync/io.c
+++ src/usr.bin/rsync/io.c
@@ -571,7 +571,7 @@ io_read_long(struct sess *sess, int fd, int64_t *val)
 	if (!io_read_int(sess, fd, &sval)) {
 		ERRX1(sess, "io_read_int");
 		return 0;
-	} else if (sval != INT32_MAX) {
+	} else if (sval != -1) {
 		*val = sval;
 		return 1;
 	}
