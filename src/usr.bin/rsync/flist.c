4ea1d6a4ececad88c4d7f8aca77520da8bbeb7ea
deraadt <>
2019-06-02 14:29:58 +0000

    spelling; from Hiltjo Posthuma

--- src/usr.bin/rsync/flist.c
+++ src/usr.bin/rsync/flist.c
@@ -53,7 +53,7 @@
 #define FLIST_TIME_SAME  0x0080 /* time is repeat */
 
 /*
- * Requied way to sort a filename list.
+ * Required way to sort a filename list.
  */
 static int
 flist_cmp(const void *p1, const void *p2)
